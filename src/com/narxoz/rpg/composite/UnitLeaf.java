a6323c26f021f2aed60e84d78926cc98781b68ba
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-03-15 04:00:38 +0500

    Added UnitLeaf CombatNode

--- src/com/narxoz/rpg/composite/UnitLeaf.java
+++ src/com/narxoz/rpg/composite/UnitLeaf.java
@@ -51,4 +51,4 @@ public abstract class UnitLeaf implements CombatNode {
     public void printTree(String indent) {
         System.out.println(indent + "- " + name + " [HP=" + health + ", ATK=" + attackPower + "]");
     }
-}
+} 
